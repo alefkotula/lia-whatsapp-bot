e96a7c6adc0f0957835498c0b8e8782b86790f4d
alefkotula <alef_kotula@hotmail.com>
2026-03-03 18:10:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const twilio = require("twilio");
-const fetch = require("node-fetch");
 const { Pool } = require("pg");
 const OpenAI = require("openai");
 
